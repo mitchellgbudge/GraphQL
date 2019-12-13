17ab5443f4b6ff1ac55754e041c88239806f3e46
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-12-13 16:45:12 -0600

    Configured supporting UI + Data Source Methods

--- RocketReserver/RocketReserver/DetailViewController.swift
+++ RocketReserver/RocketReserver/DetailViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import Apollo
 
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    var launchID: GraphQLID? {
+      didSet {
+        self.configureView()
+      }
+    }
 
 
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
+      // Update the user interface for the detail item.
+      guard
+        let label = self.detailDescriptionLabel,
+        let id = self.launchID else {
+          return
+      }
+
+      label.text = "Launch \(id)"
     }
 
     override func viewDidLoad() {
@@ -28,12 +37,7 @@ class DetailViewController: UIViewController {
         configureView()
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
+   
 
 
 }
